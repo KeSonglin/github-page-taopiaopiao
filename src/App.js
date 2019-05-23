9baa8757517c962a6710b3be41714237a28f4026
linke <15072925663@163.com>
2019-05-23 22:35:10 +0800

    excelent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './routes/home/Index';
+import User from './routes/user/Index';
+import Detail from './routes/detail/Index';
+import Seat from './routes/seat/Index';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/user" exact component={User} />
+          <Route path="/detail" exact component={Detail} />
+          <Route path="/seat" exact component={Seat} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
